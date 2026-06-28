a98ec1da0042cdd9b8c91e4b669e66b3cfac0b0c
Ram Pratap Maurya <rampratap55174@gmail.com>
2026-06-28 18:55:26 +0530

    Update mail.service.js

--- backend/src/service/mail.service.js
+++ backend/src/service/mail.service.js
@@ -1,23 +1,14 @@
 const nodemailer = require("nodemailer");
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
-console.log("RECEIVER_EMAIL:", process.env.RECEIVER_EMAIL);
-
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
-
-transporter.verify((err, success) => {
-  if (err) {
-    console.error("SMTP Verify Error:", err);
-  } else {
-    console.log("SMTP Ready");
-  }
 });
 
 module.exports = transporter;
